453b0856ee69096c49b64287e4664e0b052562b9
Gouzeman <kru864210@gmail.com>
2026-08-05 15:46:16 +0700

    big room generated complete

--- WPFGame/Level/TestLevel.cs
+++ WPFGame/Level/TestLevel.cs
@@ -15,11 +15,13 @@ namespace WPFGame.Level
             var room2 = TestRooms.Room2();
             var room3 = TestRooms.Room3();
             var room4 = TestRooms.Room4();
+            var room5 = TestRooms.Room5();
 
             rooms[room1.Id] = room1;
             rooms[room2.Id] = room2;
             rooms[room3.Id] = room3;
             rooms[room4.Id] = room4;
+            rooms[room5.Id] = room5;
 
             Connect(
                 room1,
@@ -37,6 +39,12 @@ namespace WPFGame.Level
                 room2,
                 "right_1_0",
                 room4,
+                "left_0_1");
+
+            Connect(
+                room4,
+                "right_0_1",
+                room5,
                 "left_0_1");
         }
 
